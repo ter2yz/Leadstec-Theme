a3190ba4a66c3ba0e0c983cb0d9a4acdd51de9eb
terryzzZ <zhengterry2012@gmail.com>
2023-05-07 12:27:30 +1000

    create homepage template

---
+++ page-templates/page_home.php
@@ -0,0 +1,18 @@
+<?php /* Template Name: Home Template */ ?>
+<?php get_template_part( 'template-parts/header', 'general' ); ?>
+<?php get_template_part( 'template-parts/home/hero' ); ?>
+<?php get_template_part( 'template-parts/home/stories' ); ?>
+<?php get_template_part( 'template-parts/home/solutions' ); ?>
+<?php get_template_part( 'template-parts/home/services' ); ?>
+<?php get_template_part( 'template-parts/home/clients' ); ?>
+<?php get_template_part( 'template-parts/home/why' ); ?>
+<?php get_template_part( 'template-parts/home/culture' ); ?>
+
+
+<!-- footer scripts -->
+<script src="<?php echo (get_template_directory_uri() . '/resources/js/page-home.js' ); ?>"></script>
+<script src="<?php echo (get_template_directory_uri() . '/resources/js/brand-logo-slider.js' ); ?>"></script>
+<script src="<?php echo (get_template_directory_uri() . '/resources/js/home-solutions.js' ); ?>"></script>
+<script src="<?php echo (get_template_directory_uri() . '/resources/js/gsap.js' ); ?>"></script>
+
+<?php get_template_part( 'template-parts/footer', 'general' );
