5e70de2fbebfa92aec391553b72318dd52dcf360
terryzzZ <zhengterry2012@gmail.com>
2023-05-10 02:57:26 +1000

    update solutions padding

--- template-parts/home/solutions.php
+++ template-parts/home/solutions.php
@@ -1,38 +1,37 @@
-<section class="relative w-full flex justify-center mt-20 lg:mt-40 bg-gray-200 overflow-hidden">
-    <!-- <div class="absolute h-full w-full inset-0 hidden lg:flex justify-center items-center py-12 z-30">
-        <div id="solution-mask" class="h-full aspect-square rounded-full bg-transparent"></div>
-    </div> -->
-    <div class="relative w-[91%] max-w-none lg:max-w-[1112px] mx-auto z-20">
+<section class="relative w-full mt-20 lg:mt-40 bg-gray-200 overflow-hidden">
+    <div class="relative z-20">
         <div class="w-full py-40 lg:py-60 flex flex-col items-center justify-center">
             <h2 class="capitalize text-[30px] md:text-[60px]  text-[1b1c1d] font-semibold text-center"><?php echo get_field('our_solutions')['heading']; ?></h2>
-            <div class="solutions-wrap w-full flex justify-start items-stretch mt-10 md:mt-20 overflow-x-auto no-scrollbar">
-                <?php
-                    $tools = get_field('our_solutions')['tools'];
-                    if( $tools ) {
-                        foreach( $tools as $tool) {
-                ?>
-                    <?php if($tool['url']): ?>
-                    <a href="<?php echo $tool['url']; ?>" class="w-[calc(33.33%)] md:w-[calc(25%)] lg:w-[calc(20%)] flex-none" target="_blank">
-                    <?php else: ?>
-                    <div class="w-[calc(33.33%)] md:w-[calc(25%)] lg:w-[calc(20%)] flex-none">
-                    <?php endif; ?>
-                        <div class="solution-brand w-full lg:min-w-min px-3 flex flex-col items-center h-full group">
-                            <div class="w-full h-[100px] sm:h-[150px] md:h-[200px] p-6 lg:p-10 bg-white flex-none flex justify-center items-center">
-                                <img class="w-full h-full object-center object-contain" src="<?php echo esc_url($tool['logo']['url']); ?>" alt="<?php echo $tool['logo']['alt']; ?>">
+            <div class="w-[91%] max-w-none lg:max-w-[1112px] mx-auto">
+                <div class="solutions-wrap w-full inline-flex justify-start items-stretch mt-10 md:mt-20 overflow-x-auto no-scrollbar">
+                    <?php
+                        $tools = get_field('our_solutions')['tools'];
+                        if( $tools ) {
+                            foreach( $tools as $tool) {
+                    ?>
+                        <?php if($tool['url']): ?>
+                        <a href="<?php echo $tool['url']; ?>" class="w-[calc(33.33%-0.6667rem)] md:w-[calc(25%-0.5rem)] lg:w-[calc(20%-0.4rem)] mr-2 last:mr-0 flex-none" target="_blank">
+                        <?php else: ?>
+                        <div class="w-[calc(33.33%-0.6667rem)] md:w-[calc(25%-0.5rem)] lg:w-[calc(20%-0.4rem)] mr-2 last:mr-0 flex-none">
+                        <?php endif; ?>
+                            <div class="solution-brand w-full lg:min-w-min flex flex-col items-center h-full group">
+                                <div class="w-full h-[100px] sm:h-[150px] md:h-[200px] p-3 lg:p-10 bg-white flex-none flex justify-center items-center">
+                                    <img class="w-full h-full object-center object-contain" src="<?php echo esc_url($tool['logo']['url']); ?>" alt="<?php echo $tool['logo']['alt']; ?>">
+                                </div>
+                                <div class="w-full bg-gray-100 flex justify-center grow items-center p-3 md:p-6 font-medium">
+                                    <p class="solution-brand-label opacity-50 transition duration-300 text-center group-hover:opacity-100 text-[12px] md:text-[20px] text-[1b1c1d]"><?php echo $tool['name'] ?></p>
+                                </div>
                             </div>
-                            <div class="w-full bg-gray-100 flex justify-center grow items-center py-4 md:py-6 px-6 font-medium">
-                                <p class="solution-brand-label opacity-50 transition duration-300 text-center group-hover:opacity-100 text-[12px] md:text-[20px] text-[1b1c1d]"><?php echo $tool['name'] ?></p>
-                            </div>
+                        <?php if($tool['url']): ?>
+                        </a>
+                        <?php else: ?>
                         </div>
-                    <?php if($tool['url']): ?>
-                    </a>
-                    <?php else: ?>
-                    </div>
-                    <?php endif; ?>
-                <?php
+                        <?php endif; ?>
+                    <?php
+                            }
                         }
-                    }
-                ?>
+                    ?>
+                </div>
             </div>
         </div>
     </div>
