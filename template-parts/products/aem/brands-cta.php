303f5facc59f885537a9edceba9649460e94e525
terryzzZ <zhengterry2012@gmail.com>
2023-05-10 03:24:31 +1000

    update aem brands cta

--- template-parts/products/aem/brands-cta.php
+++ template-parts/products/aem/brands-cta.php
@@ -6,17 +6,19 @@
             foreach($brands as $brand){
         ?>
             <div class="relative w-full flex flex-col items-start py-16 lg:py-32 px-16 overflow-hidden" style="background-color: <?php echo $brand['background_color'] ?>">
-                <img class="h-12 mb-12" src="<?php echo esc_url( $brand['logo']['url'] ); ?>" alt="<?php echo $brand['logo']['alt']; ?>">
-                <h3 class="relative max-w-sm font-bold text-xl text-left capitalize leading-snug text-zinc-900 mb-20 z-20"><?php echo $brand['description'] ?></h3>
-                <?php if($brand['button_url']): ?>
+                <div class="w-[250px] max-w-full h-[84px] mb-12 flex justify-start items-center">
+                    <img class="w-full h-full max-h-full max-w-full object-contain object-left" src="<?php echo esc_url( $brand['logo']['url'] ); ?>" alt="<?php echo $brand['logo']['alt']; ?>">
+                </div>
+                <h3 class="relative max-w-sm md:max-w-[350px] lg:max-w-[640px] font-bold text-[26px] text-left capitalize leading-snug text-[#1b1c1d] mb-20 z-20"><?php echo $brand['description'] ?></h3>
+                <?php if($brand['button_url'] && $brand['button_label']): ?>
                 <div class="w-60 grow flex justify-start items-end">
                     <a href="<?php echo $brand['button_url'] ?>" class="capitalize text-white flex justify-center items-center border-2 border-zinc-900 bg-zinc-900 px-6 py-2 z-20">
                     <?php echo $brand['button_label'] ?>
                     </a>
                 </div>
                 <?php endif; ?>
-                <div class="absolute h-2/3 bottom-6 -right-12 text-[26rem] font-black z-0" style="color: <?php echo $brand['decoration_letter_color'] ?>">
-                    <p><?php echo $brand['decoration_letter'] ?></p>
+                <div class="absolute h-2/3 bottom-6 -right-12 text-[26rem] font-black z-0 flex items-end" style="color: <?php echo $brand['decoration_letter_color'] ?>">
+                    <p class="leading-[0.5]"><?php echo $brand['decoration_letter'] ?></p>
                 </div>
             </div>
         <?php        
@@ -32,15 +34,19 @@
                 foreach($brands as $brand){
             ?>
                 <div class="relative w-full flex flex-col items-start py-16 lg:py-32 px-16 overflow-hidden" style="background-color: <?php echo $brand['background_color'] ?>">
-                    <img class="h-8 mb-12" src="<?php echo esc_url( $brand['logo']['url'] ); ?>" alt="<?php echo $brand['logo']['alt']; ?>">
+                    <div class="w-[181px] max-w-full h-[84px] mb-12 flex justify-start items-center">
+                        <img class="w-full h-full max-h-full max-w-full object-contain object-left" src="<?php echo esc_url( $brand['logo']['url'] ); ?>" alt="<?php echo $brand['logo']['alt']; ?>">
+                    </div>
                     <h3 class="relative max-w-sm font-bold text-xl text-left capitalize leading-snug text-zinc-900 mb-20 z-20"><?php echo $brand['description'] ?></h3>
+                    <?php if($brand['button_url'] && $brand['button_label']): ?>
                     <div class="w-60 grow flex justify-start items-end">
                         <a href="<?php echo $brand['button_url'] ?>" class="capitalize text-white flex justify-center items-center border-2 border-zinc-900 bg-zinc-900 px-6 py-2">
                             <?php echo $brand['button_label'] ?>
                         </a>
                     </div>
+                    <?php endif; ?>
                     <div class="absolute h-2/3 bottom-6 -right-12 text-[26rem] font-black z-0" style="color: <?php echo $brand['decoration_letter_color'] ?>">
-                        <p><?php echo $brand['decoration_letter'] ?></p>
+                        <p class="leading-[1]"><?php echo $brand['decoration_letter'] ?></p>
                     </div>
                 </div>
             <?php        
