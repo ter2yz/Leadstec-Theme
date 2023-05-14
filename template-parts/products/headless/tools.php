e5df264550f34ad61cafde12865061940dca9cfc
terryzzZ <zhengterry2012@gmail.com>
2023-05-14 18:19:28 +1000

    update headless tools

--- template-parts/products/headless/tools.php
+++ template-parts/products/headless/tools.php
@@ -1,17 +1,17 @@
-<section class="relative w-full flex justify-center bg-white py-16 lg:py-32">
+<section class="relative w-full flex justify-center bg-white pt-16 pb-16 lg:pt-32">
     <div class="relative w-[91%] max-w-none lg:max-w-[1112px] mx-auto z-20">
         <div class="w-full flex flex-col items-center justify-center">
             <h2 class="w-full capitalize text-[30px] md:text-[60px] text-[#1b1c1d] font-semibold mb-12 px-3"><?php echo get_field('authoring_tools_section')['heading'] ?></h2>
 
             <!-- Desktop cards -->
 
-            <div class="w-full hidden lg:flex flex-col lg:flex-row justify-center items-stretch space-y-9 space-x-0 lg:space-y-0 lg:space-x-6 xl:space-x-9">
+            <div class="w-full hidden lg:flex flex-col lg:flex-row justify-start items-stretch flex-wrap">
                 <?php
                 $tools = get_field('authoring_tools_section')['tools'];
                 if($tools) {
                     foreach($tools as $tool) {
                 ?>
-                <div class="w-full lg:w-1/3 bg-white flex flex-col px-3">
+                <div class="w-full lg:w-1/3 md:[&:nth-child(3n+1)]:pl-0 md:[&:nth-child(3n+1)]:pr-2 md:[&:nth-child(3n)]:pl-2 md:[&:nth-child(3n)]:pr-0 md:pl-1 md:pr-1 mb-6 bg-white flex flex-col">
                     <div class="w-full flex justify-center items-center">
                         <img class="w-full max-w-xs mx-auto" src="<?php echo esc_url( $tool['image'] ); ?>" alt="">
                     </div>
