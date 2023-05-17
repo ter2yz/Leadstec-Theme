a89a6670e07baaa3498da243296527f2fb4bfe88
terryzzZ <zhengterry2012@gmail.com>
2023-05-17 12:46:47 +1000

    aem - brands

--- template-parts/products/aem/brands-cta.php
+++ template-parts/products/aem/brands-cta.php
@@ -58,13 +58,13 @@
                 foreach($brands as $brand){
             ?>
                 <div class="relative w-full flex flex-col items-start py-16 lg:py-32 px-16 overflow-hidden" style="background-color: <?php echo $brand['background_color'] ?>">
-                    <div class="w-[181px] max-w-full h-[84px] mb-12 flex justify-start items-center">
+                    <div class="w-full max-w-[70%] h-[100px] mb-12 flex justify-start items-center">
                         <img class="w-full h-full max-h-full max-w-full object-contain object-left" src="<?php echo esc_url( $brand['logo']['url'] ); ?>" alt="<?php echo $brand['logo_image_alt_text']; ?>">
                     </div>
                     <h3 class="relative max-w-sm font-bold text-xl text-left capitalize leading-snug text-zinc-900 mb-20 z-20"><?php echo $brand['description'] ?></h3>
                     <?php if($brand['button_url'] && $brand['button_label']): ?>
                     <div class="w-60 grow flex justify-start items-end">
-                        <a href="<?php echo $brand['button_url'] ?>" class="capitalize text-white flex justify-center items-center border-2 border-zinc-900 bg-zinc-900 px-6 py-2">
+                        <a href="<?php echo $brand['button_url'] ?>" class="capitalize text-white flex justify-center items-center border-2 border-zinc-900 bg-zinc-900 px-6 py-2 z-20">
                             <?php echo $brand['button_label'] ?>
                         </a>
                     </div>
