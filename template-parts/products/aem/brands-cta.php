9976b4c4e89920c91fe370f0454409b9fd8b2425
terryzzZ <zhengterry2012@gmail.com>
2023-05-19 01:03:41 +1000

    aem brands

--- template-parts/products/aem/brands-cta.php
+++ template-parts/products/aem/brands-cta.php
@@ -9,10 +9,10 @@
             <div class="w-full max-w-[70%] h-[100px] mb-12 flex justify-start items-center">
                 <img class="w-full h-full max-h-full max-w-full object-contain object-left" src="<?php echo esc_url( $brands[0]['logo']['url'] ); ?>" alt="<?php echo $brands[0]['logo_image_alt_text']; ?>">
             </div>
-            <h3 class="relative max-w-sm md:max-w-[350px] lg:max-w-[640px] font-bold text-[26px] text-left capitalize leading-snug text-[#1b1c1d] mb-20 z-20"><?php echo $brands[0]['description'] ?></h3>
+            <h3 class="relative max-w-sm md:max-w-[350px] lg:max-w-[640px] font-bold text-[26px] text-left leading-snug text-[#1b1c1d] mb-20 z-20"><?php echo $brands[0]['description'] ?></h3>
             <?php if($brands[0]['button_url'] && $brands[0]['button_label']): ?>
             <div class="w-60 grow flex justify-start items-end">
-                <a href="<?php echo $brands[0]['button_url'] ?>" class="capitalize text-white flex justify-center items-center border-2 border-zinc-900 bg-zinc-900 px-6 py-2 z-20">
+                <a href="<?php echo $brands[0]['button_url'] ?>" class="text-white flex justify-center items-center border-2 border-zinc-900 bg-zinc-900 px-6 py-2 z-20">
                 <?php echo $brands[0]['button_label'] ?>
                 </a>
             </div>
@@ -32,10 +32,10 @@
                 <div class="w-full max-w-[70%] h-[100px] mb-12 flex justify-start items-center">
                     <img class="w-full h-full max-h-full max-w-full object-contain object-left" src="<?php echo esc_url( $brand['logo']['url'] ); ?>" alt="<?php echo $brand['logo_image_alt_text']; ?>">
                 </div>
-                <h3 class="relative max-w-sm md:max-w-[350px] lg:max-w-[640px] font-bold text-[26px] text-left capitalize leading-snug text-[#1b1c1d] mb-20 z-20"><?php echo $brand['description'] ?></h3>
+                <h3 class="relative max-w-sm md:max-w-[350px] lg:max-w-[640px] font-bold text-[26px] text-left leading-snug text-[#1b1c1d] mb-20 z-20"><?php echo $brand['description'] ?></h3>
                 <?php if($brand['button_url'] && $brand['button_label']): ?>
                 <div class="w-60 grow flex justify-start items-end">
-                    <a href="<?php echo $brand['button_url'] ?>" class="capitalize text-white flex justify-center items-center border-2 border-zinc-900 bg-zinc-900 px-6 py-2 z-20">
+                    <a href="<?php echo $brand['button_url'] ?>" class="text-white flex justify-center items-center border-2 border-zinc-900 bg-zinc-900 px-6 py-2 z-20">
                     <?php echo $brand['button_label'] ?>
                     </a>
                 </div>
@@ -61,10 +61,10 @@
                     <div class="w-full max-w-[70%] h-[100px] mb-12 flex justify-start items-center">
                         <img class="w-full h-full max-h-full max-w-full object-contain object-left" src="<?php echo esc_url( $brand['logo']['url'] ); ?>" alt="<?php echo $brand['logo_image_alt_text']; ?>">
                     </div>
-                    <h3 class="relative max-w-sm font-bold text-xl text-left capitalize leading-snug text-zinc-900 mb-20 z-20"><?php echo $brand['description'] ?></h3>
+                    <h3 class="relative max-w-sm font-bold text-xl text-left leading-snug text-zinc-900 mb-20 z-20"><?php echo $brand['description'] ?></h3>
                     <?php if($brand['button_url'] && $brand['button_label']): ?>
                     <div class="w-60 grow flex justify-start items-end">
-                        <a href="<?php echo $brand['button_url'] ?>" class="capitalize text-white flex justify-center items-center border-2 border-zinc-900 bg-zinc-900 px-6 py-2 z-20">
+                        <a href="<?php echo $brand['button_url'] ?>" class="text-white flex justify-center items-center border-2 border-zinc-900 bg-zinc-900 px-6 py-2 z-20">
                             <?php echo $brand['button_label'] ?>
                         </a>
                     </div>
