72649fe10a263e0b9b0e62b4ccc7d09d225e437e
terryzzZ <zhengterry2012@gmail.com>
2023-05-19 00:29:25 +1000

    update aec

--- template-parts/products/aec/products.php
+++ template-parts/products/aec/products.php
@@ -22,10 +22,10 @@
                         foreach( $stories as $story ) {
                 ?>
                     <div class="product w-full md:w-1/3 md:[&:nth-child(3n+1)]:pl-0 md:[&:nth-child(3n+1)]:pr-2 md:[&:nth-child(3n)]:pl-2 md:[&:nth-child(3n)]:pr-0 md:pl-1 md:pr-1 mb-6 overflow-hidden" data-filter="<?php echo $story['category']->slug ?>">
-                        <?php if($story['button_url']): ?>
-                        <a href="<?php echo $story['button_url'] ?>">
-                        <?php endif; ?>
-                            <div class="w-full h-full bg-white flex flex-col border border-zinc-200">
+                        <div class="w-full h-full bg-white flex flex-col border border-zinc-200">
+                            <?php if($story['button_url']): ?>
+                            <a href="<?php echo esc_url($story['button_url']); ?>" class="w-full">
+                            <?php endif; ?>
                                 <div class="w-full h-0 pt-[60%] flex-none bg-cover bg-center" aria-label="<?php echo $story['image_alt_text'] ?>" style="background-image: url(<?php echo esc_url( $story['feature_image'] ); ?>); "></div>
                                 <div class="w-full flex flex-col justify-between items-start grow p-[30px] lg:p-6 xl:p-9">
                                     <div class="w-full">
@@ -33,18 +33,18 @@
                                         <?php if ($story['category']): ?>
                                         <p class="leading-relaxed mb-[10px] text-[15px]"><?php echo esc_html( $story['category']->name ); ?></p>
                                         <?php endif; ?>
-                                        <p class="leading-relaxed text-[#9f9f9f] text-[14px] mb-12 line-clamp-3"><?php echo $story['summary']; ?></p>
+                                        <p class="leading-relaxed text-[#9f9f9f] text-[17px] mb-12 line-clamp-3"><?php echo $story['summary']; ?></p>
                                     </div>
                                     <?php if($story['button_url']): ?>
-                                    <a href="<?php echo $story['button_url'] ?>" class="capitalize text-white flex justify-center items-center text-[14px] font-bold border-2 border-[#1b1c1d] bg-[#1b1c1d] px-6 py-3">
+                                    <button class="capitalize text-white flex justify-center items-center text-[17px] font-bold border-2 border-[#1b1c1d] bg-[#1b1c1d] px-6 py-3">
                                         <?php echo $story['button_label'] ?>
-                                    </a>
+                                    </button>
                                     <?php endif; ?>
                                 </div>
-                            </div>
-                        <?php if($story['button_url']): ?>
-                        </a>
-                        <?php endif; ?>
+                            <?php if($story['button_url']): ?>
+                            </a>
+                            <?php endif; ?>
+                        </div>
                     </div>   
                 <?php
                         }
