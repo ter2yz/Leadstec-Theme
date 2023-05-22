49c6e9b4c98094e61bd2fd09e87bdcbbd8296fb0
terryzzZ <zhengterry2012@gmail.com>
2023-05-22 14:46:59 +1000

    ui - update image alt text

--- template-parts/services/web/cases.php
+++ template-parts/services/web/cases.php
@@ -38,7 +38,7 @@
                     foreach($cases as $case) {
             ?>
             <div data-index="<?php echo $slide_cases_index; ?>" class="showcase-container relative lg:absolute w-full h-full inset-0 flex flex-col justify-center items-stretch transition duration-500 opacity-100 translate-x-0 mb-10 lg:mb-0 shadow-xl rounded-xl lg:shadow-none lg:rounded-none lg:opacity-<?php echo $slide_cases_index == 0 ? "100" : "0" ?> lg:translate-x-<?php echo $slide_cases_index == 0 ? "0" : "full" ?>">
-                <img class="block w-full lg:hidden" src="<?php echo esc_url( $case['image'] ); ?>" alt="">
+                <img class="block w-full lg:hidden" src="<?php echo esc_url( $case['image'] ); ?>" alt="<?php echo $case['image_alt_text']?>">
                 <div class="w-full h-1/4 bg-zinc-900 hidden lg:flex flex-none justify-start items-center p-6">
                     <p class="text-white text-3xl font-semibold capitalize"><?php echo $case['heading']; ?></p>
                 </div>
@@ -50,7 +50,7 @@
                         </div>
                     </div>
                 </button>
-                <div class="hidden lg:block relative w-full h-full bg-top bg-cover bg-no-repeat group overflow-hidden" style="background-image: url(<?php echo esc_url( $case['image'] ); ?>)">
+                <div class="hidden lg:block relative w-full h-full bg-top bg-cover bg-no-repeat group overflow-hidden" aria-label="<?php echo $case['image_alt_text']?>" style="background-image: url(<?php echo esc_url( $case['image'] ); ?>)">
                     <div class="w-[80%] h-full bg-zinc-900/95 p-12 transition duration-500 ease-in-out ml-auto translate-x-full group-hover:translate-x-0 overflow-y-scroll">
                     <?php echo $case['description']; ?>
                     </div>
--- template-parts/services/web/hero.php
+++ template-parts/services/web/hero.php
@@ -1,10 +1,10 @@
 <section class="w-full flex justify-center items-center mb-40 relative lg:sticky top-0 z-0">
-    <div class="relative container z-20">
+    <div class="relative w-[91%] max-w-none lg:max-w-[1112px] mx-auto z-20">
         <div class="w-full flex flex-col-reverse lg:flex-row justify-start lg:justify-center items-center min-h-[600px] overflow-hidden">
             <div class="relative w-full lg:w-1/2 flex flex-col items-start z-10">
-                <h1 class="text-5xl lg:text-7xl capitalize font-semibold leading-tight"><?php echo get_field('hero_section')['heading'] ?></h1>
+                <h1 class="text-[30px] md:text-[60px] text-[#1b1c1d] font-bold leading-tight"><?php echo get_field('hero_section')['heading'] ?></h1>
                 <span class="w-20 h-3 bg-orange-500 my-5"></span>
-                <p class="leading-relaxed"><?php echo get_field('hero_section')['description'] ?></p>
+                <p class="leading-relaxed text-[15px] md:text-[20px] text-[#9f9f9f]"><?php echo get_field('hero_section')['description'] ?></p>
                 <a href="<?php echo get_field('hero_section')['button_url']; ?>" class="text-3xl w-12 h-12 mt-10 flex justify-center items-center rounded-full border-2 border-gray-900 text-gray-900">
                     <i class="fa-solid fa-chevron-down"></i>
                 </a>
