ab61303693fdda5197551a6cb07bf3373523c95b
terryzzZ <zhengterry2012@gmail.com>
2023-05-24 11:22:23 +1000

    contact - update fire event

--- resources/js/contact.js
+++ resources/js/contact.js
@@ -4,11 +4,11 @@ document.addEventListener(
         const currentURL = window.location.href;
 
         if (currentURL.includes("/en/")) {
-            location = "/en/thank-you.html";
+            location = "/en/contact-us/thank-you.html";
         } else if (currentURL.includes("/sc/")) {
-            location = "/sc/thank-you.html";
+            location = "/sc/contact-us/thank-you.html";
         } else if (currentURL.includes("/tc/")) {
-            location = "/tc/thank-you.html";
+            location = "/tc/contact-us/thank-you.html";
         } else {
             // Default location if none of the specified URLs match
             location = "/thank-you.html";
