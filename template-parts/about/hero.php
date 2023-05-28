7d4fdb4971a94f43bf43ef08dd9388435401a6fa
terryzzZ <zhengterry2012@gmail.com>
2023-05-29 00:37:40 +1000

    aboutus - update alt text

--- template-parts/about/hero.php
+++ template-parts/about/hero.php
@@ -1,9 +1,9 @@
-<section class="relative w-full flex justify-center items-center bg-cover bg-bottom py-20" style="background-image: url(<?php echo esc_url( get_field('hero_section')['background_image'] ); ?>)">
+<section class="relative w-full flex justify-center items-center bg-cover bg-bottom py-20" aria-label="<?php echo get_field('hero_section')['background_image_alt_text']; ?>" style="background-image: url(<?php echo esc_url( get_field('hero_section')['background_image'] ); ?>)">
     <div class="absolute inset-0 w-full h-full bg-black opacity-60 z-0"></div>    
     <div class="w-[91%] max-w-none lg:max-w-[1112px] mx-auto z-10">
         <div class="w-full flex flex-col-reverse lg:flex-row justify-start lg:justify-center items-center lg:min-h-[600px] overflow-hidden">
             <div class="relative w-full flex flex-col items-center z-10 text-white lg:-translate-y-20">
-                <img src="<?php echo esc_url( get_field('hero_section')['logo'] ); ?>" alt="" class="w-20 mb-6">
+                <img src="<?php echo esc_url( get_field('hero_section')['logo'] ); ?>" alt="<?php echo get_field('hero_section')['logo_image_alt_text']; ?>" class="w-20 mb-6">
                 <h1 class="text-[30px] md:text-[60px] font-bold leading-tight text-center max-w-6xl mb-5"><?php echo get_field('hero_section')['heading'] ?></h1>
                 <span class="w-20 h-3 bg-transparent my-5"></span>
                 <p class="leading-relaxed max-w-5xl text-center text-[15px] md:text-[20px]"><?php echo get_field('hero_section')['description'] ?></p>
--- template-parts/about/history.php
+++ template-parts/about/history.php
@@ -2,8 +2,8 @@
     <div class="relative w-[91%] max-w-none lg:max-w-[1112px] mx-auto z-20">
         <div class="w-full flex flex-col items-center justify-center">
             <h2 class="text-[30px] md:text-[60px] text-[#1b1c1d] font-bold mb-10 text-center"><?php echo get_field('history_section')['heading'] ?></h2>
-            <div class="w-full flex flex-col lg:flex-row justify-center items-center">
-                    <img class="slider-single-image w-full mx-auto" src="<?php echo esc_url( get_field('history_section')['image'] ); ?>" alt="">
+            <div class="w-full flex flex-col lg:flex-row justify-center items-center mb-16 lg:mb-0">
+                <img class="slider-single-image w-full mx-auto" src="<?php echo esc_url( get_field('history_section')['image'] ); ?>" alt="<?php echo get_field('history_section')['image_alt_text']; ?>">
             </div>
         </div>
     </div>
