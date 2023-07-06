546b96efca5099e2781f6db0c2010d00c46f75f7
terryzzZ <zhengterry2012@gmail.com>
2023-07-06 20:52:50 +1000

    evaluation list - update carousel styles

--- template-parts/flexibleLayouts/customer-evaluation-list.php
+++ template-parts/flexibleLayouts/customer-evaluation-list.php
@@ -1,5 +1,5 @@
 <section id="<?php echo get_sub_field('id')?>" class="relative w-full flex justify-center bg-gray-100 pt-16 pb-16 md:pb-20">
-    <div class="relative w-[91%] max-w-none lg:max-w-[1112px] mx-auto z-20">
+    <div class="slider-rl-wrapper relative w-[91%] max-w-none lg:max-w-[1112px] mx-auto z-20">
         <div class="w-full flex flex-col items-center justify-center">
             <h2 class="w-full capitalize text-[30px] md:text-[60px] text-[#1b1c1d] font-bold mb-5 md:mb-10 text-center"><?php echo get_sub_field('heading'); ?></h2>
             <div class="w-full hidden md:flex flex-row justify-start items-stretch flex-wrap">
