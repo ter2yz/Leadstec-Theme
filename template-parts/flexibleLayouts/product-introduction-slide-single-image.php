4c501f3445b715bac6e3d5bc7717db7d0de24c66
terryzzZ <zhengterry2012@gmail.com>
2023-07-07 11:49:19 +1000

    update

--- template-parts/flexibleLayouts/product-introduction-slide-single-image.php
+++ template-parts/flexibleLayouts/product-introduction-slide-single-image.php
@@ -1,4 +1,4 @@
-<section id="<?php echo get_sub_field('id')?>" class="relative w-full flex justify-center bg-white pt-10 pb-16 lg:pb-32">
+<section id="<?php echo get_sub_field('id')?>" class="relative w-full flex justify-center bg-white">
     <div class="relative w-[91%] max-w-none lg:max-w-[1112px] mx-auto z-20">
         <div class="w-full flex flex-col items-center justify-center">
             <h2 class="w-full text-center capitalize text-[30px] md:text-[60px] text-[#1b1c1d] font-semibold pt-10 pb-10 bg-white top-[67px] z-20"><?php echo get_sub_field('heading'); ?></h2>
@@ -13,9 +13,10 @@
                             foreach( $slideItems as $slideItem ) {
                     ?>
                         <div class="w-full py-16 md:py-20 flex flex-col justify-center">
-                            <h3 class="relative text-gray-900 text-[20px] md:text-[40px] font-semibold mb-6">
+                            <p class="hidden md:block absolute md:relative left-1/2 md:left-0 bottom-0 text-7xl transform origin-top-left scale-110 md:scale-150 -translate-x-1/2 md:-translate-x-0 translate-y-4 font-bold text-zinc-800/5"><?php echo $slideItem['blur_background_text'] ?></p>
+                            <h3 class="relative text-gray-900 text-[20px] md:text-[40px] font-semibold mb-6 leading-none">
                                 <?php echo $slideItem['heading'] ?>
-                                <span class="absolute left-0 bottom-0 text-7xl origin-bottom-left scale-150 font-bold text-zinc-800/5"><?php echo $slideItem['blur_background_text'] ?></span>
+                                <span class="block md:hidden absolute left-0 top-0 -translate-y-[calc(50%+5px)] text-7xl origin-bottom-left scale-110 font-bold text-zinc-800/5"><?php echo $slideItem['blur_background_text'] ?></span>
                             </h3>
                             <p class="text-[#9f9f9f] text-[15px] md:text-[20px]"><?php echo $slideItem['description'] ?></p>
                         </div>
