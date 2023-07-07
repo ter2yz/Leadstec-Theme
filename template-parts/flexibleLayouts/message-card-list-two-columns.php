68d47f29ceafc831958960561c84a525a9aad57d
terryzzZ <zhengterry2012@gmail.com>
2023-07-07 11:58:30 +1000

    update

--- template-parts/flexibleLayouts/message-card-list-two-columns.php
+++ template-parts/flexibleLayouts/message-card-list-two-columns.php
@@ -14,7 +14,7 @@
             foreach($cards as $card) {
         ?>
         <div class="w-full md:w-1/2 mx-auto group" style="--hover-color: <?php echo $card['hover_background_color'] ?>">
-            <div class="card-wrapper w-full h-full py-16 md:py-32 px-0 md:group-odd:pl-[9%] md:group-odd:pr-16 md:group-even:pl-16 md:group-even:pr-[9%] xl:group-odd:pl-[calc(100%-556px)] xl:group-even:pr-[calc(100%-556px)] border-y md:odd:border-r border-zinc-100 bg-white transition duration-300 group">
+            <div class="card-wrapper w-full h-full py-8 md:py-16 px-0 md:group-odd:pl-[9%] md:group-odd:pr-16 md:group-even:pl-16 md:group-even:pr-[9%] xl:group-odd:pl-[calc(100%-556px)] xl:group-even:pr-[calc(100%-556px)] border-y md:odd:border-r border-zinc-100 bg-white transition duration-300 group">
                 <div class="w-[91%] md:w-full mx-auto h-full flex flex-col items-start">
                     <h3 class="font-bold text-[20px] md:text-[26px] px-3 mb-3 capitalize text-[#1b1c1d] text-center md:text-left <?php echo $card['hover_text_theme'] == 'dark' ? "group-hover:text-[#1b1c1d]" : "group-hover:text-white" ?>"><?php echo $card['heading'] ?></h3>
                     <p class="leading-relaxed text-[15px] md:text-[20px] px-3 md:max-w-md text-[#9f9f9f] text-left <?php echo $card['hover_text_theme'] == 'dark' ? "group-hover:text-[#9f9f9f]" : "group-hover:text-white" ?>">
