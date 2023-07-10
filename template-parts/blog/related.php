a24698006530b6cb740ddd64cdff9d0e5dbb70d1
terryzzZ <zhengterry2012@gmail.com>
2023-07-10 12:07:47 +1000

    update blog

--- template-parts/blog/related.php
+++ template-parts/blog/related.php
@@ -11,9 +11,11 @@ $recent_posts = get_posts(array(
     'meta_key'  => 'post_date',
     'orderby' => array( 'meta_value' => 'DESC' ), 
 ));
-if ($recent_posts){
 ?>
 <div class="w-full pb-40">
+<?php
+if ($recent_posts){
+?>
     <div class="w-[91%] max-w-none lg:max-w-[1112px] mx-auto">
         <h2 class="w-full text-[30px] md:text-[60px] text-[#1b1c1d] font-bold mt-[44px] mb-[20px] md:mb-[44px]"><?php echo get_field('similar_cases_heading') ?></h2>
         <div class="relative w-full pb-16">
@@ -47,5 +49,5 @@ if ($recent_posts){
             </div>
         </div>
     </div>
+    <?php } ?>
 </div>
-<?php } ?>
