c05ddfcb8796b8b852372baf73cc338943cbe54c
terryzzZ <zhengterry2012@gmail.com>
2023-07-21 14:43:12 +1000

    404 styles

--- page-templates/page_404.php
+++ page-templates/page_404.php
@@ -17,8 +17,8 @@
         <div class="w-60 h-60 rounded-full mb-6 overflow-hidden">
             <img src="<?php echo get_field('image', $current_page->ID)['url'] ?>" alt="<?php echo get_field('image_alt_text', $current_page->ID); ?>" class="w-full h-full object-cover object-center">
         </div>
-        <h2 class="text-[#1b1c1d] text-[30px] md:text-[60px] mb-3"><?php echo get_field('heading', $current_page->ID) ?></h2>
-        <p class="text-[#9f9f9f] text-[15px] md:text-[20px] text-center mb-6"><?php echo get_field('description', $current_page->ID) ?></p>
+        <h2 class="text-[#1b1c1d] text-[30px] md:text-[60px] mb-3 text-center leading-[1.2]"><?php echo get_field('heading', $current_page->ID) ?></h2>
+        <p class="text-[#9f9f9f] text-[15px] md:text-[20px] text-center mb-6 leading-[1.2]"><?php echo get_field('description', $current_page->ID) ?></p>
         <a href="<?php echo get_field('button_url', $current_page->ID) ?>" class="text-[16px] text-white flex justify-center items-center border-2 border-zinc-900 bg-zinc-900 px-6 py-2">
             <?php echo get_field('button_label', $current_page->ID) ?>
         </a>
