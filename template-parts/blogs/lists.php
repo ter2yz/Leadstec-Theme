94e6dd7a2be704c173a70f1bbcc6b6dc09815935
terryzzZ <zhengterry2012@gmail.com>
2023-07-24 16:53:13 +1000

    update pagination

--- template-parts/blogs/lists.php
+++ template-parts/blogs/lists.php
@@ -1,3 +1,26 @@
+<?php 
+function processPagination($paginationArray, $prev_text, $next_text) {
+    $output = new stdClass();
+    $output->prev = null;
+    $output->next = null;
+
+    foreach ($paginationArray as $index => $pageItem) {
+        preg_match('/href="(.*?)"/', $pageItem, $matches);
+        $url = isset($matches[1]) ? $matches[1] : null;
+        $title = strip_tags($pageItem);
+        console_log($title);
+        if ($title === $prev_text) {
+            $output->prev = $url;
+        }
+        if ($title === $next_text) {
+            $output->next = $url;
+        }
+    }
+
+    return $output;
+}
+
+?>
 <section class="relative w-full z-20">
     <div class="w-full flex flex-col-reverse md:flex-row justify-center items-start md:min-h-[600px]">
         <div id="<?php echo get_field('recent_section_id'); ?>" class="w-[91%] max-w-none lg:max-w-[1112px] mx-auto md:w-4/12 md:min-h-screen md:sticky md:top-12 bg-white md:bg-gray-100 pt-6 pb-40 lg:px-12 lg:pt-12">
@@ -40,39 +63,87 @@
         <div id="<?php echo get_field('all_posts_section_id'); ?>" class="hidden md:block w-full md:w-8/12 px-6 pt-6 pb-40 lg:px-12 lg:pt-12">
             <div class="w-full flex flex-col md:flex-row flex-wrap justify-start items-stretch transition opacity-100 px-6 lg:px-0">
                 <?php
-                $allPosts = $recent_posts;
-                if ($allPosts) {
-                    foreach($allPosts as $singlePost) {
-                        $current_taxonomies = get_post_taxonomies($singlePost);
-                        $categories = get_the_terms($singlePost, $current_taxonomies[0]);
-                        if(get_field('post_date', $singlePost->ID)){
-                            $postDate = DateTime::createFromFormat( 'Ymd', get_field('post_date', $singlePost->ID) );
-                            $dateStr = $postDate->format('j F Y');
-                            if ($current_type == "blogs_sc" || $current_type == "blogs_tc") {
-                                $dateStr = $postDate->format('Y年n月j日');; 
-                            }
-                        } else {
-                            $dateStr = "";
+                $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+                $loop = new WP_Query( array(
+                    'numberposts' => -1,
+                    'post_type' => $current_type,
+                    'meta_key'  => 'post_date',
+                    'orderby' => array( 'meta_value' => 'DESC' ), 
+                    'posts_per_page' => 10,
+                    'paged' => $paged,
+                ));
+                if ( $loop->have_posts() ) {
+                    while ( $loop->have_posts() ) : $loop->the_post();
+                    $current_taxonomies = get_post_taxonomies($post);
+                    $categories = get_the_terms($post, $current_taxonomies[0]);
+                    if(get_field('post_date', $post->ID)){
+                        $postDate = DateTime::createFromFormat( 'Ymd', get_field('post_date', $post->ID) );
+                        $dateStr = $postDate->format('j F Y');
+                        if ($current_type == "blogs_sc" || $current_type == "blogs_tc") {
+                            $dateStr = $postDate->format('Y年n月j日');; 
                         }
+                    } else {
+                        $dateStr = "";
+                    }
+                // $allPosts = $recent_posts;
+                // if ($allPosts) {
+                //     foreach($allPosts as $singlePost) {
+                //         $current_taxonomies = get_post_taxonomies($singlePost);
+                //         $categories = get_the_terms($singlePost, $current_taxonomies[0]);
+                //         if(get_field('post_date', $singlePost->ID)){
+                //             $postDate = DateTime::createFromFormat( 'Ymd', get_field('post_date', $singlePost->ID) );
+                //             $dateStr = $postDate->format('j F Y');
+                //             if ($current_type == "blogs_sc" || $current_type == "blogs_tc") {
+                //                 $dateStr = $postDate->format('Y年n月j日');; 
+                //             }
+                //         } else {
+                //             $dateStr = "";
+                //         }
                 ?>
                 <div class="w-full lg:w-1/2 lg:odd:pl-0 lg:odd:pr-3 lg:even:pl-3 lg:even:pr-0 overflow-hidden mb-6">
                     <div class="w-full h-full bg-white flex flex-col border border-zinc-200">
-                        <a href="<?php echo get_permalink($singlePost) ?>" class="w-full h-0 pt-[60%] flex-none bg-cover bg-center" aria-label="<?php echo get_field('image_alt_text', $singlePost->ID); ?>" style="background-image: url(<?php echo esc_url( get_field('feature_image', $singlePost->ID)['url'] ); ?>); "></a>
+                        <a href="<?php echo get_permalink($post) ?>" class="w-full h-0 pt-[60%] flex-none bg-cover bg-center" aria-label="<?php echo get_field('image_alt_text', $post->ID); ?>" style="background-image: url(<?php echo esc_url( get_field('feature_image', $post->ID)['url'] ); ?>); "></a>
                         <div class="w-full flex flex-col justify-between items-start grow p-3 md:p-6 xl:p-9">
                             <div class="w-full">
-                                <h3 class="font-bold text-xl text-left mb-3"><?php echo get_field('title', $singlePost->ID) ?></h3>
+                                <h3 class="font-bold text-xl text-left mb-3"><?php echo get_field('title', $post->ID) ?></h3>
                                 <p class="leading-relaxed mb-3"><?php echo $categories[0]->name ?><?php echo $categories[0]&&$dateStr ? " • " : "" ?><?php echo $dateStr; ?></p>
-                                <p class="leading-relaxed text-[#9f9f9f] text-[14px] line-clamp-3 mb-6"><?php echo get_field('short_description', $singlePost->ID) ?></p>
+                                <p class="leading-relaxed text-[#9f9f9f] text-[14px] line-clamp-3 mb-6"><?php echo get_field('short_description', $post->ID) ?></p>
                             </div>
-                            <a href="<?php echo get_permalink($singlePost) ?>" class="text-sky-600"><?php echo get_field('learn_more_label');?></a>
+                            <a href="<?php echo get_permalink($post) ?>" class="text-sky-600"><?php echo get_field('learn_more_label');?></a>
                         </div>
                     </div>
                 </div>
                 <?php
-                    }
+                    endwhile;
+                ?>
+                </div>
+                <?php
+                    
+                    $total_pages = $loop->max_num_pages;
+                
+                    if ($total_pages > 1){
+                
+                        $current_page = max(1, get_query_var('paged'));
+                
+                        $pagination = paginate_links(array(
+                            'base' => html_entity_decode( get_pagenum_link(1) ) . '%_%',
+                            'format' => '?paged=%#%',
+                            'current' => $current_page,
+                            'total' => $total_pages,
+                            'type' => 'array',
+                            'prev_text' => __('prev'),
+                            'next_text' => __('next'),
+                        ));
+                        $pagination_object = processPagination($pagination, 'prev', 'next');
+                    }    
                 }
                 ?>
-            </div>
+                <?php if($total_pages >1): ?>
+                <div class="flex">
+                    <a class="py-2 px-3 rounded-lg border-solid border mr-2 <?php echo $pagination_object->prev ? 'border-blue-500 text-blue-500' : 'text-gray-200 border-gray-200 pointer-events-none cursor-default' ?>" href="<?php echo $pagination_object->prev ?>" ><</a>
+                    <a class="py-2 px-3 rounded-lg border-solid border <?php echo $pagination_object->next ? 'border-blue-500 text-blue-500' : 'text-gray-200 border-gray-200 pointer-events-none cursor-default' ?>" href="<?php echo $pagination_object->next ?>" >></a>
+                </div>
+                <?php endif; ?>
         </div>
         <div id="<?php echo get_field('all_posts_section_id'); ?>" class="ms-fw-container block md:hidden w-full overflow-auto md:touch-none cursor-grab no-scrollbar bg-gray-100">
             <div class="ms-container w-[91%] max-w-none lg:max-w-[1112px] mx-auto">
