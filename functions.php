0dd19349bd393f72e856e11af3c1474179fff686
terryzzZ <zhengterry2012@gmail.com>
2023-08-25 12:28:48 +1000

    update

--- functions.php
+++ functions.php
@@ -196,7 +196,7 @@ add_action( 'after_setup_theme', 'register_navwalker' );
 
 add_filter( 'do_redirect_guess_404_permalink', '__return_false' );
 
-add_filter( 'wpcf7_use_recaptcha_net', '__return_false' );
+add_filter( 'wpcf7_use_recaptcha_net', '__return_true' );
 
 function add_query_vars_filter( $vars ) {
   $vars[] = "category";
