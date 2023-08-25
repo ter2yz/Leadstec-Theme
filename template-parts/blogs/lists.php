b8f9c099d40e184b1eb67d8bb48a774604a23fb8
terryzzZ <zhengterry2012@gmail.com>
2023-08-25 15:05:11 +1000

    refactor blog lists page

--- template-parts/blogs/lists.php
+++ template-parts/blogs/lists.php
@@ -102,8 +102,8 @@ console_log($categories);
                 </div>
             </div>
         </div>
-        <div id="<?php echo get_field('all_posts_section_id', $current_post->ID); ?>" class="hidden md:block w-full md:w-8/12 px-6 pt-6 pb-10 lg:px-12 lg:pt-12">
-            <div class="w-full flex flex-col md:flex-row flex-wrap justify-start items-stretch transition opacity-100 px-6 lg:px-0">
+        <div id="<?php echo get_field('all_posts_section_id', $current_post->ID); ?>" class=" w-full md:w-8/12 px-6 pt-6 pb-10 lg:px-12 lg:pt-12">
+            <div class="w-full flex flex-col md:flex-row flex-wrap justify-start items-stretch transition opacity-100 px-0 md:px-6 lg:px-0">
                 <?php
                 $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
                 $loop = new WP_Query( array(
@@ -115,7 +115,7 @@ console_log($categories);
                     'posts_per_page' => 10,
                     'paged' => $paged,
                 ));
-                if ( $loop->have_posts() ) {
+                if ( $loop->have_posts() ){
                     while ( $loop->have_posts() ) : $loop->the_post();
                     $current_taxonomies = get_post_taxonomies($post);
                     $categories = get_the_terms($post, $current_taxonomies[0]);
@@ -129,7 +129,7 @@ console_log($categories);
                         $dateStr = "";
                     }
                 ?>
-                <a href="<?php echo get_permalink($post) ?>" class="w-full lg:w-1/2 lg:odd:pl-0 lg:odd:pr-3 lg:even:pl-3 lg:even:pr-0 overflow-hidden mb-6">
+                <a href="<?php echo get_permalink($post) ?>" class="hidden md:block w-full lg:w-1/2 lg:odd:pl-0 lg:odd:pr-3 lg:even:pl-3 lg:even:pr-0 overflow-hidden mb-6">
                     <div class="w-full h-full bg-white flex flex-col border border-zinc-200">
                         <div class="w-full h-0 pt-[60%] flex-none bg-cover bg-center" aria-label="<?php echo get_field('image_alt_text', $post->ID); ?>" style="background-image: url(<?php echo esc_url( get_field('feature_image', $post->ID)['url'] ); ?>); "></div>
                         <div class="w-full flex flex-col justify-between items-start grow p-3 md:p-6 xl:p-9">
@@ -142,10 +142,24 @@ console_log($categories);
                         </div>
                     </div>
                 </a>
+                <a href="<?php echo get_permalink($post) ?>" class="flex md:hidden w-full h-full bg-white flex-col border border-zinc-200 mb-6">
+                    <div class="w-full h-0 pt-[60%] flex-none bg-cover bg-center" aria-label="<?php echo get_field('image_alt_text', $post->ID); ?>" style="background-image: url(<?php echo esc_url( get_field('feature_image', $post->ID)['url'] ); ?>); "></div>
+                    <div class="w-full flex flex-col justify-between items-start grow p-[30px] lg:p-6 xl:p-9">
+                        <div class="w-full">
+                            <h3 class="font-bold text-[20px] text-[#1b1c1d] text-left mb-3"><?php echo get_field('title', $post->ID) ?></h3>
+                            <p class="leading-relaxed text-[#1b1c1d] text-[15px] mb-3"><?php echo $categories[0]->name ?><?php echo $categories[0]&&$dateStr ? " • " : "" ?><?php echo $dateStr; ?></p>
+                            <p class="leading-relaxed text-[#9f9f9f] text-[14px] md:text-[17px] mb-6 line-clamp-3"><?php echo get_field('short_description', $post->ID) ?></p>
+                        </div>
+                        <?php if (get_field('learn_more_label', $current_post->ID)): ?>
+                        <button class="text-white flex justify-center text-[14px] md:text-[17px] font-bold items-center border-2 border-[#1b1c1d] bg-[#1b1c1d] px-6 py-3">
+                            <?php echo get_field('learn_more_label', $current_post->ID);?>
+                        </button>
+                        <?php endif; ?>
+                    </div>
+                </a>
                 <?php
                     endwhile;
                 ?>
-                </div>
                 <?php
                     
                     $total_pages = $loop->max_num_pages;
@@ -174,56 +188,13 @@ console_log($categories);
                     }    
                 }
                 ?>
+                </div>
                 <?php if($total_pages >1): ?>
-                <div class="flex px-6 lg:px-0">
+                <div class="flex px-0 md:px-6 lg:px-0">
                     <a class="py-2 px-3 rounded-lg border-solid border mr-2 <?php echo $pagination_object->prev ? 'border-blue-500 text-blue-500' : 'text-gray-200 border-gray-200 pointer-events-none cursor-default' ?>" href="<?php echo $pagination_object->prev ?>" ><</a>
                     <a class="py-2 px-3 rounded-lg border-solid border <?php echo $pagination_object->next ? 'border-blue-500 text-blue-500' : 'text-gray-200 border-gray-200 pointer-events-none cursor-default' ?>" href="<?php echo $pagination_object->next ?>" >></a>
                 </div>
                 <?php endif; ?>
-        </div>
-        <div id="<?php echo get_field('all_posts_section_id', $current_post->ID); ?>" class="ms-fw-container block md:hidden w-full overflow-auto md:touch-none cursor-grab no-scrollbar bg-gray-100">
-            <div class="ms-container w-[91%] max-w-none lg:max-w-[1112px] mx-auto">
-                <div class="ms-wrap inline-flex flex-nowrap my-[20px] md:my-[40px]">
-                    <?php
-                    $allPosts = get_posts(array(
-                        'numberposts' => -1,
-                        'post_type' => $current_type,
-                        'meta_key'  => 'post_date',
-                        'orderby' => array( 'meta_value' => 'DESC' ), 
-                    ));
-                    if ($allPosts) {
-                        foreach($allPosts as $singlePost) {
-                            $current_taxonomies = get_post_taxonomies($singlePost);
-                            $categories = get_the_terms($singlePost, $current_taxonomies[0]);
-                            if(get_field('post_date', $singlePost->ID)){
-                                $postDate = DateTime::createFromFormat( 'Ymd', get_field('post_date', $singlePost->ID) );
-                                $dateStr = $postDate->format('j F Y');
-                                if ($current_type == "blogs_sc" || $current_type == "blogs_tc") {
-                                    $dateStr = $postDate->format('Y年n月j日');; 
-                                }
-                            } else {
-                                $dateStr = "";
-                            }
-                    ?>
-                    <div class="w-[290px] mr-3 last:mr-0">
-                        <div class="w-full h-full bg-white flex flex-col border border-zinc-200">
-                            <a href="<?php echo get_permalink($singlePost) ?>" class="w-full h-0 pt-[60%] flex-none bg-cover bg-center" aria-label="<?php echo get_field('image_alt_text', $singlePost->ID); ?>" style="background-image: url(<?php echo esc_url( get_field('feature_image', $singlePost->ID)['url'] ); ?>); "></a>
-                            <div class="w-full flex flex-col justify-between items-start grow p-3 md:p-6 xl:p-9">
-                                <div class="w-full">
-                                    <h3 class="font-bold text-xl text-left mb-3"><?php echo get_field('title', $singlePost->ID) ?></h3>
-                                    <p class="leading-relaxed mb-3"><?php echo $categories[0]->name ?><?php echo $categories[0]&&$dateStr ? " • " : "" ?><?php echo $dateStr; ?></p>
-                                    <p class="leading-relaxed text-[#9f9f9f] text-[14px] line-clamp-3 mb-6"><?php echo get_field('short_description', $singlePost->ID) ?></p>
-                                </div>
-                                <a href="<?php echo get_permalink($singlePost) ?>" class="text-sky-600"><?php echo get_field('learn_more_label', $current_post->ID);?></a>
-                            </div>
-                        </div>
-                    </div>
-                    <?php
-                        }
-                    }
-                    ?>
-                </div>
-            </div>
         </div>
         <div class="w-[91%] mx-auto flex md:hidden flex-col">
             <h3 class="font-bold text-xl text-left mb-[20px]"><?php echo get_field('categories_heading', $current_post->ID);?></h3>
@@ -231,11 +202,13 @@ console_log($categories);
                 <a href="<?php echo $base_list_url; ?>?category=all" class="border <?php echo strtolower(get_query_var('category')) == 'all' ? 'text-orange-500 border-orange-500' : 'text-[#9f9f9f] border-zinc-400/25' ?> text-[15px] font-bold rounded-lg px-6 py-2 mr-3 whitespace-nowrap mb-3"><?php echo get_field('all_posts_heading', $current_post->ID); ?></a>
                 <?php
                     console_log($mobileCategories);
+                    if($mobileCategories){
                     foreach($mobileCategories as $cat){
                 ?>
                     <a href="<?php echo $base_list_url; ?>?category=<?php echo $cat->slug; ?>" class="border <?php echo $current_taxo == $cat->name ? 'text-orange-500 border-orange-500' : 'text-[#9f9f9f] border-zinc-400/25' ?> text-[15px] font-bold rounded-lg px-6 py-2 mr-3 mb-3 whitespace-nowrap"><?php echo $cat->name; ?></a>
                 <?php
-                    }   
+                    }
+                }
                 ?>
             </div>
         </div>
