6aecb3fae69ad0a7eb2f92266e44752aecd853c3
terryzzZ <zhengterry2012@gmail.com>
2023-09-26 23:19:34 +1000

    fix js error

--- resources/js/brand-logo-slider.js
+++ resources/js/brand-logo-slider.js
@@ -1,24 +1,24 @@
-const splideClient = new Splide("#solutions-partners .splide", {
-    type: "loop",
-    drag: "free",
-    focus: "center",
-    arrows: false,
-    pagination: false,
-    perPage: 5,
-    autoScroll: {
-        speed: 1,
-    },
-    breakpoints: {
-        640: {
-            perPage: 3,
+const spSplideContainer = document.querySelector("#solutions-partners .splide");
+spSplideContainer &&
+    new Splide("#solutions-partners .splide", {
+        type: "loop",
+        drag: "free",
+        focus: "center",
+        arrows: false,
+        pagination: false,
+        perPage: 5,
+        autoScroll: {
+            speed: 1,
         },
-        768: {
-            perPage: 4,
+        breakpoints: {
+            640: {
+                perPage: 3,
+            },
+            768: {
+                perPage: 4,
+            },
+            1024: {
+                perPage: 4,
+            },
         },
-        1024: {
-            perPage: 4,
-        },
-    },
-});
-
-splideClient.mount(window.splide.Extensions);
+    }).mount(window.splide.Extensions);
